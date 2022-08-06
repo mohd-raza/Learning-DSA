ca755eb5e5676571d7ff43e441ac8012c7a9391d
Mohammed <mohdraza.syed@gmail.com>
2022-08-07 01:00:30 +0530

    Day-8

---
+++ 06_Matrices/Diagonal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+struct Matrix
+{
+    int A[10];
+    int n;
+};
+void Set(struct Matrix *m, int i, int j, int x)
+{
+    if (i == j)
+    {
+        m->A[i - 1] = x;
+    }
+}
+int Get(struct Matrix m, int i, int j)
+{
+    if (i == j)
+        return m.A[i - 1];
+    else
+        return 0;
+}
+void Display(struct Matrix m)
+{
+    int i, j;
+    for (i = 0; i < m.n; i++)
+    {
+        for (j = 0; j < m.n; j++)
+        {
+            if (i == j)
+                cout << m.A[i - 1] << " ";
+            else
+                cout << " 0 ";
+        }
+        cout << endl;
+    }
+}
+int main(int argc, char const *argv[])
+{
+    struct Matrix m;
+    m.n = 4;
+    Set(&m, 1, 1, 5);
+    Set(&m, 2, 2, 1);
+    Set(&m, 3, 3, 7);
+    Set(&m, 4, 4, 10);
+    cout << Get(m, 2, 2);
+    Display(m);
+    return 0;
+}
